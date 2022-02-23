8016f10ccb6d5da1cd6a7f392d85395597709524
ramana <ramanamurthy.bure@gmail.com>
2022-02-23 15:50:33 +0530

    Updates with Stream Filter Test Method

--- src/javacorepractice/com/StreamsTests.java
+++ src/javacorepractice/com/StreamsTests.java
@@ -5,29 +5,40 @@ import java.util.ArrayList;
 import org.testng.annotations.Test;
 
 public class StreamsTests {
-	@Test
-	public void regular() {
-		
-		ArrayList<String> arls=new ArrayList<String>();
-		arls.add("Ramana");
-		arls.add("Murthy");
-		arls.add("Gowri");
-		arls.add("Karthi");
-		arls.add("Anudeep");
-		arls.add("Chinnari");
-		arls.add("Swamy");
-		arls.add("Raja");
-		arls.add("Ranga");
-		
-		System.out.println("**********************************************************");		
-		System.out.println(arls);
-		System.out.println("**********************************************************");
-		for(int i=0;i<arls.size();i++)
-		{
-			System.out.println(arls.get(i));
+
+	public ArrayList<String> namesList = new ArrayList<String>();
+
+	public StreamsTests() {
+		namesList.add("Ramana");
+		namesList.add("Murthy");
+		namesList.add("Gowri");
+		namesList.add("Karthi");
+		namesList.add("Anudeep");
+		namesList.add("Chinnari");
+		namesList.add("Swamy");
+		namesList.add("Raja");
+		namesList.add("Ranga");
+	}
+
+	@Test(priority = 1)
+	public void FilterWithoutUsingStream() {
+		System.out.println("->Printing the arraylist");
+		System.out.println(namesList);
+		System.out.println("->Finding the total no of Names started with letter 'R'");
+		int count = 0;
+		for (int i = 0; i < namesList.size(); i++) {
+			if (namesList.get(i).startsWith("R")) {
+				count = count + 1;
+			}
 		}
-		System.out.println("**********************************************************");
-		
-		System.out.println("*********************Streamms*******************************");
+		System.out.println(count + " Names started with first letter'R'");
+		System.out.println("->Solving the same using Streams in Java");
+
+	}
+
+	@Test(priority = 2)
+	public void FliterWithStream() {
+		long count = namesList.stream().filter(s -> s.startsWith("R")).count();
+		System.out.println(count + " Names started with first letter'R'");
 	}
 }
