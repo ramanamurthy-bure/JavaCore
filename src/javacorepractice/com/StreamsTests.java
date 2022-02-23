91fe500dbe33f49dba0ed0bf4cc08be80232d003
ramana <ramanamurthy.bure@gmail.com>
2022-02-23 15:15:17 +0530

    Java Core practice exaples

---
+++ src/javacorepractice/com/StreamsTests.java
@@ -0,0 +1,31 @@
+package javacorepractice.com;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.Test;
+
+public class StreamsTests {
+	@Test
+	public void regular() {
+		
+		ArrayList<String> arls=new ArrayList<String>();
+		arls.add("Ramana");
+		arls.add("Murthy");
+		arls.add("Gowri");
+		arls.add("Karthi");
+		arls.add("Anudeep");
+		arls.add("Chinnari");
+		arls.add("Swamy");
+		arls.add("Raja");
+		arls.add("Ranga");
+		
+		System.out.println("**********************************************************");		
+		System.out.println(arls);
+		System.out.println("**********************************************************");
+		for(int i=0;i<arls.size();i++)
+		{
+			System.out.println(arls.get(i));
+		}
+		System.out.println("**********************************************************");
+	}
+}
