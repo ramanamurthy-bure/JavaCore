aa29d8b7573f9a67f8cdf09e1e3cce22462a602f
ramanamurthy-bure <ramanamurthy.bure@gmail.com>
2022-10-28 12:56:36 +0530

    Test

---
+++ src/javacorepractice/com/Test.java
@@ -0,0 +1,17 @@
+package javacorepractice.com;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter circle radius: ");
+		int radius = in.nextInt();
+		double area = Math.PI * radius * radius;
+		System.out.println("Area of circle : " + area);
+
+	}
+
+}
